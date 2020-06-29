51944c076dc13142a99d69ba008a05460411b92e
Son <sonlequang98@gmail.com>
2020-06-29 20:07:53 +0700

    fix image

--- Frontend/app/Post.php
+++ Frontend/app/Post.php
@@ -19,6 +19,6 @@ class Post extends Model
     }
     public function image()
     {
-        return $this->belongsTo('Frontend\image');
+        return $this->belongsTo('Frontend\Image');
     }
 }
