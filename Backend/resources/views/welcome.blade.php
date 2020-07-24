128efae3c67d2d1e43ab38510fb026c15d48c2a6
SonLQ <kull18utoi@gmail.com>
2020-07-24 14:52:31 +0700

    fix viewport

--- Backend/resources/views/welcome.blade.php
+++ Backend/resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <!-- Meta, title, CSS, favicons, etc. -->
         <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
         <meta name="viewport" content="width=device-width, initial-scale=1">
          <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
         <!-- Bootstrap core CSS -->
